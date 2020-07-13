571ec22215c95605f50da8d7bf5a9421eeda2551
juni-lee <juni22246@gmail.com>
2020-07-14 08:41:09 +0900

    L05_CIFAR-10_tutorial.py 완료

--- L05_CIFAR-10_tutorial.py
+++ L05_CIFAR-10_tutorial.py
@@ -16,22 +16,22 @@ def imshow(img):
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 == torch.nn.Conv2d(3, 6, 5)
-        self.pool = torch.nn.MaxPool2d(2, 2)
+        self.conv1 = torch.nn.Conv2d(3, 6, 5)
         self.conv2 = torch.nn.Conv2d(6, 16, 5)
+        self.pool = torch.nn.MaxPool2d(2, 2)
         self.fc1 = torch.nn.Linear(16 * 5 * 5, 120)
         self.fc2 = torch.nn.Linear(120, 84)
         self.fc3 = torch.nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.pool(torch.nn.functional.relu(self.conv1(x)))
-        x = self.pool(torch.nn.functional.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = torch.nn.functional.relu(self.fc1(x))
-        x = torch.nn.functional.relu(self.fc2(x))
-        x = self.fc3(x)
+        x1 = self.pool(torch.nn.functional.relu(self.conv1(x)))
+        x2 = self.pool(torch.nn.functional.relu(self.conv2(x1)))
+        x3 = x2.view(-1, 16 * 5 * 5)
+        x4 = torch.nn.functional.relu(self.fc1(x3))
+        x5 = torch.nn.functional.relu(self.fc2(x4))
+        x6 = self.fc3(x5)
 
-        return x
+        return x6
 
 
 
@@ -54,19 +54,18 @@ if __name__ == "__main__":
 
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-
     """
     Let us show some of the training images, for fun
     """
-    # get some random training images
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
-
-    # show images
-    imshow(torchvision.utils.make_grid(images))
-
-    # print labels
-    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    # # get some random training images
+    # dataiter = iter(trainloader)
+    # images, labels = dataiter.next()
+    #
+    # # show images
+    # imshow(torchvision.utils.make_grid(images))
+    #
+    # # print labels
+    # print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
 
     """
@@ -83,7 +82,7 @@ if __name__ == "__main__":
     
     Let's use a Classification Cross-Entropy loss and SGD with momentum
     """
-    criterion = torch.nn.CrossEntropyLOss()
+    criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 
@@ -93,3 +92,86 @@ if __name__ == "__main__":
     This is when things start to get interesting. We simply have to loop over our data iterator,
     and feed the inputs to the network and optimize.
     """
+    net.train()
+    for epoch in range(1):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss = running_loss + loss.item()
+            if i % 500 == 499:
+                print('[%d, %5d] loss: %.3f' % (epoch + 1,
+                                                i + 1,
+                                                running_loss / 2000))
+                running_loss = 0.0
+
+    print('Finished Training')
+
+    """
+    Let's quickly save our trained model
+    """
+    # PATH = "./cifar_net.pth"
+    # torch.save(net.state_dict(), PATH)
+
+
+    """
+    5. Test the network on the test data
+    
+    We will check this by predicting the class label that the neural network outputs,
+    and checking it against the ground-truth.
+    If the prediction is correct, we add the sample to the list of correct predictions
+    """
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+
+    # imshow(torchvision.utils.make_grid(images))
+    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+    # net = Net()
+    # net.load_state_dict(torch.load(PATH))
+
+    net.eval()
+    with torch.no_grad():
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+    correct = 0
+    total = 0
+    net.eval()
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total = total + labels.size(0)
+            correct = correct + (predicted == labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    net.eval()
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] = class_correct[label] + c[i].item()
+                class_total[label] = class_total[label] + 1
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
